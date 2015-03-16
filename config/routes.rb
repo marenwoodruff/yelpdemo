c0dca2ce963cec792f144e0ac4ec7136f3c3bdc4
MarenWoodruff <wren403@gmail.com>
2015-03-15 22:27:16 -0700

    added Restaurants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Yelpdemo::Application.routes.draw do
+  resources :restaurants
+
   get "pages/about"
   get "pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'restaurants#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
