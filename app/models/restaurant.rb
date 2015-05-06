670600b079355abf0cb6f4761d1e5c23638fdaf7
Maren Woodruff <wren403@gmail.com>
2015-05-05 22:21:58 -0400

    prepared app for s3

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100>" }, :default_url => "missing.png"
+	has_attached_file :image, 
+    :styles          => {:medium => "300x300>", :thumb => "100x100>"}, 
+    :default_url     => "missing.png",
+    :convert_options => { :all => '-background white -flatten +matte' }
+    #:styles => { :medium => "300x300", :thumb => "100x100>" }, :default_url => "missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
+
+
+
+
