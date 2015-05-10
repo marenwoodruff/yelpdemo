d193881ec9ada20ca9c63aa04854c5ba13260b7d
Maren Woodruff <wren403@gmail.com>
2015-05-10 17:49:12 -0400

    added aws-sdk configuration + changed README doc.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ActiveRecord::Base
     :styles          => {:medium => "300x300>", :thumb => "100x100>"}, 
     :default_url     => "missing.png",
     :convert_options => { :all => '-background white -flatten +matte' }
-    #:styles => { :medium => "300x300", :thumb => "100x100>" }, :default_url => "missing.png"
+    
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
 
