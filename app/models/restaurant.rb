6ec6360e31d9b5e725dc59349a6a2afe72166959
Maren Woodruff <wren403@gmail.com>
2015-08-26 12:36:26 -0400

    added devise_invitation section to save for later

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
 	has_attached_file :image, 
     :styles          => {:medium => "300x300>", :thumb => "100x100>"}, 
-    :default_url     => "missing.png",
+    :default_url     => "none",
     :convert_options => { :all => '-background white -flatten +matte' }
     
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
